8e8bf2ca46004726b3fd40fbcd55999aafc05bf1
vkshay3400 <akshayp2208@gmail.com>
2020-04-07 19:59:11 +0530

    Added: Designing pattern example

---
+++ src/main/java/com/corejava/designpattern/adapterexample/adapter/Payment.java
@@ -0,0 +1,15 @@
+package com.corejava.designpattern.adapterexample.adapter;
+
+public class Payment {
+    public String type;
+    public double amount;
+
+    public Payment(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public void pay() {
+        System.out.println(type + " " + amount + "$");
+    }
+}
